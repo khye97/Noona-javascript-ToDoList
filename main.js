6c6fe11c39bd8fca9660fafee59eb304f3e514f4
Hyejin Kim <khyejin08@naver.com>
2024-07-02 18:20:05 +0900

    todo list item 추가 코드 수정

--- main.js
+++ main.js
@@ -2,7 +2,11 @@
 // + 버튼을 클릭하면 할 일이 추가된다
 // 엔터 버튼을 눌러도 할 일이 추가된다
 // delete 버튼을 누르면 할 일이 삭제된다
-// check 버튼을 누르면 할일이 종료되고 취소선이 생긴다 
+// check 버튼을 누르면 할일이 종료되고 취소선이 생긴다
+// 1. check 버튼을 클릭하면 false -> true가 된다
+// 2. true이면 끝난걸로 간주하고 취소선 보여주기
+// 3. false이면 안끝난걸로 간주하고 그대로 
+
 // 체크 버튼을 한번 더 누르면 취소됐던게 다시 돌아온다
 // 진행중, 끝남 탭을 누르면 언더바가 이동한다
 // 끌남 탭은 끝난 아이템만, 진행중 탭은 진행중인 아이템만 나온다 
@@ -10,13 +14,21 @@
 
 let userInput = document.getElementById('todo-input');
 let addBtn = document.getElementById('add-btn');
+let checkBtn = document.getElementById('check');
+let deleteBtn = document.getElementById('delete');
+let todoList = document.querySelector('.todo-list');
 let inputValueList = [];
+
 
 addBtn.addEventListener('click', addTask);
 
 function addTask (){
-  let taskContent = userInput.value;
-  inputValueList.push(taskContent);
+  let task = {
+    taskContent: userInput.value, // 유저가 실제로 입력한 값
+    id: generateRandomId(),  // 랜덤한 id
+    isComplete: false,  // 완료 여부 
+  }
+  inputValueList.push(task);
   console.log(inputValueList);
   render();
 }
@@ -26,13 +38,29 @@ function render (){
   for (let i = 0; i < inputValueList.length; i++){
     resultHTML += `<div class="todo-list">
                       <div class="todo-item">
-                        ${inputValueList[i]}
+                        <span>${inputValueList[i].taskContent}</span>
                       </div>
                       <div class="btn-box">
-                        <button type="button" id="check">check</button>
-                        <button type="button" id="delete">delete</button>
+                        <button onclick="toggleComplete('${inputValueList[i].id}')" type="button" id="check" class="icon-btn"><i class="fa-solid fa-check"></i></button>
+                        <button onclick="deleteTask()" type="button" id="delete" class="icon-btn"><i class="fa-regular fa-trash-can"></i></button>
                       </div>
                     </div>`
-    document.querySelector('.todo-board').innerHTML = resultHTML;                
+                    // 조심해야 할 것
+                    // <button onclick="toggleComplete('${inputValueList[i].id}')" type="button" id="check" class="icon-btn"><i class="fa-solid fa-check"></i></button> 이 코드에서 toggleComplete('') 안에 따옴표를 넣는 것은 이게 함수이고 매개변수로 들어갈 값이 문자열(${inputValueList[i].id})이기 때문 
   }
+  document.querySelector('.todo-board').innerHTML = resultHTML;
 }
+
+// false->true / true->false 바꾸는 함수
+function toggleComplete (id){
+  console.log(id);
+}
+
+function deleteTask (){
+  console.log('삭제됨'); // 잘 작동함
+}
+
+function generateRandomId (){
+  return "_" + Math.random().toString(36).substring(2, 9);
+}
+
